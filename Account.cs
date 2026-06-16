2d78b46253f0feb0be43b50a145727fd6867633d
717824f212-svg <717824f212@kce.ac.in>
2026-06-16 20:17:08 +0530

    Add files via upload

---
+++ Account.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banking_System
+{
+    internal class Account
+    {
+        public int AccountNumber { get; set; }
+        public string AccountHolderName { get; set; }
+        public double Balance { get; set; }
+
+        public void Deposit(double amount)
+        {
+            Balance += amount;
+            Console.WriteLine($"Deposited Amount : {amount}");
+            Console.WriteLine($"Current Balance : {Balance}");
+        }
+
+        public virtual void Withdraw(double amount)
+        {
+            Console.WriteLine("Withdraw Method");
+        }
+    }
+}
+
